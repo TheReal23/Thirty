7845a0a4f35070cfb53afc579022a49b3b30c9a9
TheReal23 <65376048+TheReal23@users.noreply.github.com>
2020-06-07 09:28:14 -0700

    add chance to add tile value by 2

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = Math.random() < 0.9 ? 1 : 2;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -127,8 +127,8 @@ GameManager.prototype.move = function (direction) {
 
         // Only one merger per row traversal?
         if (next && next.value === tile.value && !next.mergedFrom) {
-          var multiply = Math.random() < 0.986 ? 2 : 4;
-          var merged = new Tile(positions.next, tile.value * multiply);
+          var multiply = Math.random() < 0.986 ? 1 : 2;
+          var merged = new Tile(positions.next, tile.value + multiply);
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
@@ -140,8 +140,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 16384 tile
-          if (merged.value === 16384) self.won = true;
+          // The mighty 30 tile
+          if (merged.value === 30) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
